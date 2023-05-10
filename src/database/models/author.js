9652af38d5946efe6733aa7183f973cb9525da75
Erwin Nahue <erwinnahue@gmail.com>
2023-05-09 22:13:03 -0500

    Create migrade Author

---
+++ src/database/models/author.js
@@ -0,0 +1,49 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Author extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Author.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      birthdate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      biography: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Author',
+    }
+  );
+  return Author;
+};
